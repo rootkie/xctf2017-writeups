106d85fe39c4d068b8ae011f9097c7f43c4423e4
Ragul Balaji <ragulbaby@gmail.com>
2017-06-17 16:50:16 +0800

    finals temp

---
+++ finals/Time to Take a Dump - 200/brute.py
@@ -0,0 +1,16 @@
+import serial
+
+ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
+
+FINDME = "vI2QjIy8q7mEvZ6MzMnLoM6FoJeejZuC9f/="
+PASS = ""
+maxchar = ''
+maxmatch = 0
+
+while True:
+	maxmatch = 0
+	maxchar = ''
+	for cn in range(32,127):
+		testpass = PASS + chr(cn)
+		ser.write(testpass+'\n')
+		print testpass
